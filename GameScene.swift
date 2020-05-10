8a674e1582177218d6806b43c49ef41bfc605629
insksenia <58752878+insksenia@users.noreply.github.com>
2020-05-09 19:17:04 -0700

    Create GameScene.swift

---
+++ GameScene.swift
@@ -0,0 +1,52 @@
+# Ksenia-s-Game
+
+import SpriteKit
+import GameplayKit
+ 
+class GameScene: SKScene {
+    
+    
+    let player = SKSpriteNode(imageNamed: "perry" )
+    
+    
+    
+    let bulletSound = SKAction.playSoundFileNamed("BulletSoundEffect", waitForCompletion: false)
+    
+    override func didMove(to view: SKView) {
+    let background = SKSpriteNode(imageNamed: "background")
+        background.size = self.size
+        background.position = CGPoint (x: self.size.width/2, y: self.size.height/2)
+        background.zPosition = 0
+        self.addChild(background)
+       
+        player.setScale(0.3)
+        player.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
+        player.zPosition = 2
+        self.addChild(player)
+    }
+    func fireBullet() {
+      let bullet = SKSpriteNode(imageNamed: "bullet")
+        bullet.setScale(1)
+        bullet.position = player.position
+        bullet.zPosition = 1
+        self.addChild(bullet)
+        let moveBullet = SKAction.moveTo(y: self.size.height + bullet.size.height, duration: 1)
+        let deleteBullet = SKAction.removeFromParent()
+        let bulletSequence = SKAction.sequence([bulletSound, moveBullet, deleteBullet])
+        bullet.run(bulletSequence)
+        
+    }
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        fireBullet()
+    }
+    
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch: AnyObject in touches{
+            let pointOfTouch = touch.location(in: self)
+            let previousPointOfTouch = touch.previousLocation(in: self)
+            let amountDragged = pointOfTouch.x - previousPointOfTouch.x
+            player.position.x += amountDragged
+        }
+    }
+}
