40a503e9aedb1574b89d136e3a3e4983292ce98a
insksenia <58752878+insksenia@users.noreply.github.com>
2020-05-10 23:58:36 -0700

    Update GameScene.swift

--- GameScene.swift
+++ GameScene.swift
@@ -12,6 +12,33 @@ class GameScene: SKScene {
     
     let bulletSound = SKAction.playSoundFileNamed("BulletSoundEffect", waitForCompletion: false)
     
+    
+    func random() -> CGFloat {
+        return CGFloat(Float(arc4random())/0xFFFFFFFF)
+    }
+    func random(min: CGFloat, max: CGFloat) -> CGFloat {
+        return random() * (max - min) + min
+    }
+    
+    
+    let gameArea: CGRect
+    override init(size: CGSize) {
+        
+        
+        let maxAspectRatio: CGFloat = 16.0/9.0
+        let playableWidth = size.height / maxAspectRatio
+        let margin = (size.width - playableWidth) / 2
+        gameArea = CGRect(x: margin, y: 0, width: playableWidth, height: size.height)
+        
+        
+        super.init(size: size)
+    
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func didMove(to view: SKView) {
     let background = SKSpriteNode(imageNamed: "background")
         background.size = self.size
@@ -23,10 +50,24 @@ class GameScene: SKScene {
         player.position = CGPoint(x: self.size.width/2, y: self.size.height/5)
         player.zPosition = 2
         self.addChild(player)
+        startNewLevel()
     }
+    
+    func startNewLevel(){
+        
+        let spawn = SKAction.run(spawnEnemy)
+        let waitToSpawn = SKAction.wait(forDuration: 1)
+        let spawnSequence = SKAction.sequence([spawn, waitToSpawn])
+        let spawnForever = SKAction.repeatForever(spawnSequence)
+        self.run(spawnForever)
+    }
+    
+    
+    
+    
     func fireBullet() {
       let bullet = SKSpriteNode(imageNamed: "bullet")
-        bullet.setScale(1)
+        bullet.setScale(0.2)
         bullet.position = player.position
         bullet.zPosition = 1
         self.addChild(bullet)
@@ -36,8 +77,38 @@ class GameScene: SKScene {
         bullet.run(bulletSequence)
         
     }
+    
+    
+    func spawnEnemy(){
+        let randomXStart = random(min: gameArea.minX, max: gameArea.maxX)
+        let randomXEnd = random(min: gameArea.minX, max: gameArea.maxX)
+        
+        let startPoint = CGPoint(x: randomXStart, y: self.size.height * 1.2)
+        let endPoint = CGPoint(x: randomXEnd, y: -self.size.height * 0.2)
+        
+        let enemy = SKSpriteNode(imageNamed: "enemy")
+        enemy.setScale(0.2)
+        enemy.position = startPoint
+        enemy.zPosition = 2
+        self.addChild(enemy)
+        
+        let moveEnemy = SKAction.move(to: endPoint, duration: 5)
+        let deleteEnemy = SKAction.removeFromParent()
+        let enemySequence = SKAction.sequence([moveEnemy, deleteEnemy])
+        enemy.run(enemySequence)
+        
+        
+        let dx = endPoint.x - startPoint.x
+        let dy = endPoint.y - startPoint.y
+        
+        let amoutToRotate = atan2(dy, dx)
+        enemy.zRotation = amoutToRotate
+    }
+    
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
         fireBullet()
+        spawnEnemy()
     }
     
     
@@ -47,6 +118,14 @@ class GameScene: SKScene {
             let previousPointOfTouch = touch.previousLocation(in: self)
             let amountDragged = pointOfTouch.x - previousPointOfTouch.x
             player.position.x += amountDragged
+            
+            if player.position.x > gameArea.maxX - player.size.width/2{
+                player.position.x = gameArea.maxX - player.size.width/2
+            }
+            if player.position.x < gameArea.minX + player.size.width/2{
+                player.position.x = gameArea.minX + player.size.width/2
+                
+            }
         }
     }
 }
